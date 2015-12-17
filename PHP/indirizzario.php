29dd6ca20a35cf5ccb9d855373da5207cdef1d52
Raffone17 <raffoespo@gmail.com>
2015-12-17 11:49:27 +0100

    gestione oggettiphp

--- PHP/indirizzario.php
+++ PHP/indirizzario.php
@@ -3,22 +3,39 @@
 <body>
 
     <form   method="post">
+            <p>nome</p>
             <input name="nome"      type="text" /> 
-            <input name="cognome"   type="text" /> 
+            <p>cognome</p>
+             <input name="cognome"   type="text" />
+						<p>indirizzo</p> 
             <input name="indirizzo" type="text" />  
-            <input name="telefono"  type="text" /> 
-            <input type="submit"    value="go"  />  
+            <p>telefono</p> 
+             <input name="telefono"  type="text" /> 
+             
+             <input type="submit"    value="INVIO"  />  
    </form>
 
 </html>
-<?php
-         if  (isset($_post["nome"])      &&
-				      isset($_post["cognome"])   &&
-              isset($_post["indirizzo"]) &&
-              isset($_post["telefono"])){
+<?php    include"libreria_classi.php";   
+
+         if  (isset($_POST["nome"])      &&
+				      isset($_POST["cognome"])   &&
+              isset($_POST["indirizzo"]) &&
+              isset($_POST["telefono"])){
+							$indirizzo= new Indirizzo();
+              $indirizzo->nome=$_POST["nome"];
+ 							$indirizzo->cognome=$_POST["cognome"];
+              $indirizzo->indirizzo=$_POST["indirizzo"];
+							$indirizzo->telefono=$_POST["telefono"];
+
+
+              // echo $indirizzo->nome;
+              // echo $indirizzo->cognome;
+  						// echo $indirizzo->indirizzo;
+              // echo $indirizzo->telefono;
+    
+               echo $indirizzo->visualizza();
 							
-
-
 							}
 
 ?>
--- PHP/libreria_classi.php
+++ PHP/libreria_classi.php
@@ -1,9 +1,29 @@
-// file libreria classi php
+
 <?
+// file libreria classi php
 	class Indirizzo {
              	public $nome;
               public $cognome; 
  	            public $indirizzo;
-              public $tel; 
+              public $telefono; 
+
+        			public  function nomeCompleto(){
+							        $n=$this->nome . " " .  $this->cognome;
+                      return $n; 
+											}              
+            
+							public  function visualizza(){
+							        $n=$this->nomeCompleto();
+                      $n=$n."<br/>"; 
+											$n=$n. $this->indirizzo;
+  										$n=$n."<br/>"; 
+											$n=$n. $this->telefono;
+
+                      return $n; 
+                          	
+
+							}
+
+
 }
 ?>
