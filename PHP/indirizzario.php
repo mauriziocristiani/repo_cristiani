d733d160d4215e747de3c8f45cb4f7c5e851d300
Raffone17 <raffoespo@gmail.com>
2015-12-17 10:15:19 +0100

    classi

---
+++ PHP/indirizzario.php
@@ -0,0 +1,32 @@
+<html>
+
+<body>
+
+    <form   method="post">
+            <input name="nome"      type="text" /> 
+            <input name="cognome"   type="text" /> 
+            <input name="indirizzo" type="text" />  
+            <input name="telefono"  type="text" /> 
+            <input type="submit"    value="go"  />  
+   </form>
+
+</html>
+<?php
+         if  (isset($_post["nome"])      &&
+				      isset($_post["cognome"])   &&
+              isset($_post["indirizzo"]) &&
+              isset($_post["telefono"])){
+							
+
+
+							}
+
+?>
+
+
+
+
+
+
+
+</body>
---
+++ PHP/libreria_classi.php
@@ -0,0 +1,9 @@
+// file libreria classi php
+<?
+	class Indirizzo {
+             	public $nome;
+              public $cognome; 
+ 	            public $indirizzo;
+              public $tel; 
+}
+?>
