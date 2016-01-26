dec08cdfcca3b08e627a935b97a54e68042d2383
Raffone17 <raffoespo@gmail.com>
2016-01-26 14:18:50 +0100

    esercizio javascript beccaris  26.01.2016

--- PHP/ciclofor.php
+++
@@ -1,7 +0,0 @@
-<?php
-//stampa il tuo nome al contrario
-$nome="dario";
-  for($i=strlen($nome)-1 ;$i>=0 ;$i--){
-  echo $nome[$i];
-}
-?>
--- PHP/classi.php
+++
@@ -1,9 +0,0 @@
-// file libreria classi php
-<?
-	class Indirizzo {
-             	public $nome;
-              public $cognome; 
- 	            public $indirizzo;
-              public $tel; 
-}
-?>
